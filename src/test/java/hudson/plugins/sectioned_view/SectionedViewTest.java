9429eb8b765a91acad6b2cc33695b64723799526
Steve Hill <sghill.code@outlook.com>
2024-08-07 05:09:11 +0000

    Modernize to Jenkins 2.440.3
    Use this link to re-run the recipe: https://app.moderne.io/recipes/org.openrewrite.jenkins.ModernizePlugin?organizationId=SmVua2lucyBDSQ%3D%3D
    
    Co-authored-by: Moderne <team@moderne.io>

--- src/test/java/hudson/plugins/sectioned_view/SectionedViewTest.java
+++ src/test/java/hudson/plugins/sectioned_view/SectionedViewTest.java
@@ -9,11 +9,11 @@ import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
-import com.gargoylesoftware.htmlunit.html.HtmlButton;
-import com.gargoylesoftware.htmlunit.html.HtmlElement;
-import com.gargoylesoftware.htmlunit.html.HtmlForm;
-import com.gargoylesoftware.htmlunit.html.HtmlInput;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.htmlunit.html.HtmlButton;
+import org.htmlunit.html.HtmlElement;
+import org.htmlunit.html.HtmlForm;
+import org.htmlunit.html.HtmlInput;
+import org.htmlunit.html.HtmlPage;
 import hudson.markup.RawHtmlMarkupFormatter;
 import hudson.model.Descriptor;
 import hudson.model.ItemGroup;
